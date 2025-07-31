8100bd34318c493a3efe33d6601dd2341e1ae01b
Kydollar <rizkywidodocuan@gmail.com>
2025-08-01 01:48:00 +0700

    feat: remove home template and redirect to dashboard on load

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,15 +1,6 @@
-<template>
-  <div class="home">
-    <h1>This is an home page</h1>
-  </div>
-</template>
+<script setup>
+import { useRouter } from 'vue-router'
 
-<style>
-@media (min-width: 1024px) {
-  .home {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+const router = useRouter()
+router.replace({ name: '/app/dashboard' })
+</script>
